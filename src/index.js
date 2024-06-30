5e2e4d9e2ea678e0357122c516470020e6e6510c
Lautaro Muller <lautiimuller@gmail.com>
2024-06-30 20:11:08 -0300

    commented lines removed

--- src/index.js
+++ src/index.js
@@ -5,25 +5,22 @@ import stockPrice from './data/stock-price.js';
 const app = express();
 const port = 3000;
 
-// Ruta para obtener productos
 app.get('/api/products', (req, res) => {
     res.json(products);
 });
 
-// Ruta para obtener stock y precio por SKU
 app.get('/api/stock-price/:sku', (req, res) => {
     const sku = req.params.sku;
     const stockPriceInfo = stockPrice[sku];
 
     if (stockPriceInfo) {
-        // Convertir el precio de centavos a dólares
         const priceInDollars = (stockPriceInfo.price / 100).toFixed(2);
         res.json({ ...stockPriceInfo, price: priceInDollars });
     } else {
-        res.status(404).send('SKU no encontrado');
+        res.status(404).send('SKU not found');
     }
 });
 
 app.listen(port, () => {
-    console.log(`Servidor ejecutándose en http://localhost:${port}`);
+    console.log(`Server running in http://localhost:${port}`);
 });
